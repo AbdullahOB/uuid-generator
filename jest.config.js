2828a67614d86a014092ec2eb55488e625302693
Abdullah Obaid <abdullahobeid2@gmail.com>
2023-09-30 19:31:14 +0300

    feat: added testing to actions

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
 }
